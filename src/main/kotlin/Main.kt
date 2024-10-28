18744ad1363c62770dd73fa3f27765664a33c33f
максим Трушин <mask.trushin.06@bk.ru>
2024-10-28 10:33:27 +0300

    fcom

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main() {
+    //Ex6
+    var age = 16
+    println(age)
+    age = 30
+    print(age)
+
+    //Ex7
+    //4610
+    //5600
+    //4601
+
+    //Ex8
+    //(5 * 3) - ((4 / 2) * 2)
+
+    //Ex9
+    val a: Double = 52.0
+    val b: Double = 25.0
+    val average = (a + b) / 2
+
+    //Ex10
+    val farenheit: Double = 1489.0
+    val celcius = (farenheit / 1.8) - 32
+
+    //Ex11
+    val position = 51
+    val row = position / 8
+    val column = position % 8
+
+    //Ex13
+    val x1: Double = 5.0
+    val y1: Double = 5.0
+    val x2: Double = 8.0
+    val y2: Double = 8.0
+
+    val distance = sqrt((x2 - x2).pow(2.0) + (y2 - y1).pow(2.0))
+}
